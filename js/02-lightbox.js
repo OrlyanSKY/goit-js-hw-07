966e04fac039741213437166e97186c2d63e03c2
Denis Orlyansky <000slim001@gmail.com>
2022-07-31 17:20:02 +0300

    fix/add li in ul

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,10 +3,10 @@ import { galleryItems } from "./gallery-items.js";
 function createGallaryItem(array) {
   return array
     .map(({ preview, original, description }) => {
-      return `
-  <a class="gallery__item" href="${original}">
+      return `<li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>
+</a></li>
+  
 `;
     })
     .join("");
